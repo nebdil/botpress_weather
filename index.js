bb424974d4da994effadce7853519279fe2b7e64
nebdil <dilannebioglu@gmail.com>
2017-12-18 11:29:13 -0500

    suggestion improved

--- index.js
+++ index.js
@@ -5,18 +5,15 @@ module.exports = function(bp) {
   // Listens for a first message (this is a Regex)
   // GET_STARTED is the first message you get on Facebook Messenger
   bp.hear(/GET_STARTED|hello|hi|test|hey|holla/i, (event, next) => {
-    event.reply('#welcome') // See the file `content.yml` to see the block
+    event.reply('#welcome')
   })
 
-  // You can also pass a matcher object to better filter events
+  // if leaving
   bp.hear({
     type: /message|text/i,
     text: /exit|bye|goodbye|quit|done|leave|stop/i
   }, (event, next) => {
-    event.reply('#goodbye', {
-      // You can pass data to the UMM bloc!
-      reason: 'unknown'
-    })
+    event.reply('#goodbye')
   })
 
   // if wants some suggestions
@@ -35,16 +32,18 @@ module.exports = function(bp) {
     let lon = event.raw.payload.coordinates.long;
     let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.W_API}`
     request(url, function (err, response, body) {
-      if(err){
+      if (err){
         console.log('error:', error);
       } else {
         let weather = JSON.parse(body)
-        console.log('description: ', weather.weather[0].description)
-        console.log('temp min: ', weather.main.temp_min)
-        console.log('temp max: ', weather.main.temp_max)
-        console.log('city: ', weather.name)
-        event.reply('#askLocation_reply', { weather: weather })
+        if (weather.main.temp_max >= 10) {
+          event.reply('#askLocation_replyHot', { weather: weather })
+        } else if (weather.main.temp_max >= -5) {
+          event.reply('#askLocation_replyWarm', { weather: weather })
+        } else {
+          event.reply('#askLocation_replyCold', { weather: weather })
+        }
       }
-    });
+    })
   })
 }
